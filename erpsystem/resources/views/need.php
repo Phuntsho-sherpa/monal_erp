177b37eb3023047e16d947430c419fdf5b1a72de
77231876 <wangmor275@gmail.com>
2023-04-22 10:51:55 +0600

    update

---
+++ erpsystem/resources/views/need.php
@@ -0,0 +1,15 @@
+<DOCTYPE HTML>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" 
+integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/7.1.0/esm/ionicons.min.js">
+<meta charset="UTF-8">
+    <title>icon</title>
+<h3>All you need</h3>
+  </head>
+  <body>
+  <i class="fa-solid fa-sack-dollar"></i>
+  </body>
+
+</html>
