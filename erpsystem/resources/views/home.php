a59ca047aa282bffde59d118621146b537880a87
Phuntsho-sherpa <nimphuntsho4@gmail.com>
2023-04-22 11:36:22 +0600

    update

---
+++ erpsystem/resources/views/home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Phuntsho</title>
+</head>
+
+<body>
+    <header>
+        <div class="navbar">
+            <div class="logo"> <img src="logo.png" alt="My Website Logo">
+            </div>
+            <ul class="links">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#services">Services</a></li>
+            </ul>
+            <a href="#" class=action_btn>Get Started</a>
+            <div class="toggle_btn">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </div>
+        <div class="dropdown_menu">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#services">Services</a></li>
+            <a href="#" class=action_btn>Get Started</a>
+        </div>
+    </header>
+    <main>
+        <section id="hero">
+<b><h1 style="font-size: 70px; color: black;">Welcome</h1></b>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,<br>
+     placeat enim iure ipsa sequi harum consequatur! Qui officiis adipisci <br>
+     laboriosam ratione possimus quae.</p>
+     <a href="#" class=action_btn2>Get_Started</a>
+        </section>
+        <div class="btn3">
+        <a href="#" class=action_btn3>Contact_Us</a>
+    </div>
+   
+    </main>
+
+    <div class="img1">
+        <img src="image/erps.jpeg" > 
+    </div>
+</body>
+<script src="main.js"></script>
+
+</html>
