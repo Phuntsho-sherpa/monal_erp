5ae73b7c0775a5fc6bf1261713dd3f348b7fd23d
Phuntsho-sherpal <nimphuntsho4@gmail.com>
2023-05-08 11:10:36 +0600

    update

--- erpsystem/resources/views/home.php
+++ erpsystem/resources/views/home.php
@@ -51,7 +51,7 @@ integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJ
             </h2><br>
 <p> A software system that helps you run your entire business, supporting 
               automation and processes in finance, human resources, manufacturing, supply chain, services, procurement, and more.</p>
-            
+              <p><a class="btn" href="https://www.oracle.com/erp/what-is-erp/implementation/">CREATE YOUR ERP</a></p>
 
           </div>
           <div class="home-computer-container">
@@ -60,16 +60,16 @@ integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJ
         </section>
         <img src="https://live.staticflickr.com/6004/6011613496_537a047292_b.jpg" alt="" class="erp">
   <br>
-<div class="h1" style="font-family:Roboto", sans-serif;> 
+  <div class="sub-title"> 
  <p>ALL YOU NEED.</p>
-  <u><h6>Lets build your Future with us.</h6></u>
+  <h2>Lets build your Future with us.</h2>
 </div>
  
 
 
 <div class="icon-grid">
   <div class="icon-with-text">
-    <i class="fa-solid fa-sack-dollar" id="fin" style="color: #f8b704;"></i>
+   <a href="#" ><i class="fa-solid fa-sack-dollar" class="icons" style="color: #f8b704;"></i> </a>
     <h4>Financial Management</h4>
     <p>At the core of ERP are the financial <br>
     modules, including  generalledger,accounts <br>
@@ -78,61 +78,67 @@ integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJ
   </div>
  
   <div class="icon-with-text">
-    <i class="fa-solid fa-eye" style="color: #3584e4;"></i>
+  <a href="#" > <i class="fa-solid fa-eye" class="icons" style="color: #3584e4;"></i></a>
     <h4>CRM</h4>
     <p>Retain more customers by optimizing the sales <br>
     process. Track the leads, opportunites...</p>
     <a href="/crm">learn more<span style="color: #3584e4;">&#8594;</span></a>
   </div>
   <div class="icon-with-text">
-  <i class="fa-solid fa-headphones-simple" style="color: #9141ac;"></i>
+  <a href="#" >  <i class="fa-solid fa-headphones-simple" class="icons" style="color: #9141ac;"></i></a>
   <h4>Help Desk</h4>
     <p>Deliver a better service experience with an intuitive 
     issue tracker and an integrated knowledge base..</p>
     <a href="/help">learn more<span style="color: #3584e4;">&#8594;</span></a>
   </div>
   <div class="icon-with-text">
-  <i class="fa-solid fa-list-check" style="color: #ec2370;"></i>
+  <a href="#" > <i class="fa-solid fa-list-check" class="icons" style="color: #ec2370;"></i></a>
   <h4>Inventory Management</h4>
     <p>Inventory Management and control is the <br>combination of information technology and business  
     processes of maintaining </p>
     <a href="/inventory">learn more<span style="color: #3584e4;">&#8594;</span></a>
   </div>
   <div class="icon-with-text">
-  <i class="fa-regular fa-handshake" style="color: #39d7c1;"></i>
+  <a href="#" ><i class="fa-regular fa-handshake" class="icons" style="color: #39d7c1;"></i></a>
   <h4>Human Resources</h4>
     <p>Human resource management ERP modules should enhance 
     the employee experience – from initial recruitment to time tracking</p>
     <a href="/human">learn more<span style="color: #3584e4;">&#8594;</span></a>
   </div>
   <div class="icon-with-text">
-  <i class="fa-solid fa-network-wired" style="color: #b5835a;"></i>
+  <a href="#" > <i class="fa-solid fa-network-wired" class="icons" style="color: #b5835a;"></i></a>
   <h4>Asset Management</h4>
     <p>Asset management is a systematic process of <br>
     developing, operating, maintaining, upgrading,</p>
     <a href="/asset">learn more<span style="color: #3584e4;">&#8594;</span></a>
   </div>
   <div class="icon-with-text">
-  <i class="fa-solid fa-layer-group"style="color: #f36f03;"></i> </i>
+  <a href="#" ><i class="fa-solid fa-layer-group" class="icons" style="color: #f36f03;"></i> </i></a>
   <h4>Point of Sales</h4>
     <p>The selling of goods which will take care of<br> schemes & discounts, and even claims.</p>
     <a href="/sale">learn more<span style="color: #3584e4;">&#8594;</span></a>
   </div>
   <div class="icon-with-text">
-  <i class="fa-solid fa-truck" style="color: #9a9996;"></i>
+  <a href="#" ><i class="fa-solid fa-truck" class="icons" style="color: #9a9996;"></i></a>
   <h4>Fleet Management</h4>
     <p>POL management of pool vehicles and load vehicles 
     including the maintainese cost and the tyres changes for the vechicle.</p>
     <a href="/fleet">learn more<span style="color: #3584e4;">&#8594;</span></a>
   </div>
   <div class="icon-with-text">
-  <i class="fa-solid fa-book-open" style="color: #5eff2d;"></i>
+  <a href="#" ><i class="fa-solid fa-book-open" class="icons" style="color: #5eff2d;"></i></a>
   <h4>Enterprise Resource Planning</h4>
     <p>An Enterpirse is a group of people with a common goal, which has certail resources at its disposal to achieve that </p>
     <a href="/enterprise">learn more<span style="color: #3584e4;">&#8594;</span></a>
   </div>
 </div>
      
+
+
+
+
+
+
 
 
 
